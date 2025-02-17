87f64b33305863685efc1172d82d02455eb3fe38
Michael Ang <contactmang@gmail.com>
2025-02-17 16:28:56 +0400

    Rotate display. Wait on startup to try to avoid I2C errors

--- CircuitPython/demo_slider_key.py
+++ CircuitPython/demo_slider_key.py
@@ -7,47 +7,59 @@ NOTE: These devices both have address 0x30 by default.
       You need to cut jumper A3 on the back of the NeoSlider to change
       its address to 0x38
 """
+import board
+# Flip display so buttons are on right
+display = board.DISPLAY
+display.rotation = 180
+
 print("demo_slider_key")
 
-import board
+# Set false to skip printing debug messages - faster performance
+serial_debug = True
+
+import time
 from rainbowio import colorwheel
 from adafruit_seesaw.seesaw import Seesaw
 from adafruit_seesaw.analoginput import AnalogInput
 from adafruit_seesaw import neopixel
 from adafruit_neokey.neokey1x4 import NeoKey1x4
 
+# Give I2C devices a chance to turn on
+time.sleep(0.5)
 
-# NeoSlider Setup
+## NeoSlider Setup
 i2c = board.I2C()  # uses board.SCL and board.SDA
-#i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 #neoslider = Seesaw(i2c, 0x30) # Default address
 neoslider_address = 0x38 # Address with A3 cut, to avoid conflicts with Neokey at 0x30
 print("Connecting to Neoslider at %x" % neoslider_address)
-neoslider = Seesaw(i2c, neoslider_address)
-
-potentiometer = AnalogInput(neoslider, 18)
-pixels = neopixel.NeoPixel(neoslider, 14, 4, pixel_order=neopixel.GRB)
+neoslider = Seesaw(i2c, neoslider_address) # Object to talk to NeoSlider hardware
+fader = AnalogInput(neoslider, 18) # Tracks position of slider
+old_fader_value = fader.value # Keep track of old value so we can respond to changes
+pixels = neopixel.NeoPixel(neoslider, 14, 4, pixel_order=neopixel.GRB) # For changing colors
 
 ## Set up Neokey
 print("Setting up NeoKey")
 # Create a NeoKey object using existing i2c bus
 neokey = NeoKey1x4(i2c, addr=0x30) # Default address
 
-# Set false to skip printing debug messages - faster performance
-serial_debug = False
 
-
-def potentiometer_to_color(value):
-    """Scale the potentiometer values (0-1023) to the colorwheel values (0-255)."""
+def fader_to_color(value):
+    """Scale the fader values (0-1023) to the colorwheel values (0-255)."""
     return value / 1023 * 255
 
 
+print("Entering main loop")
 while True:
     ### NeoSlider handling
-    if serial_debug:
-        print(potentiometer.value)
-    # Fill the pixels a color based on the position of the potentiometer.
-    pixels.fill(colorwheel(potentiometer_to_color(potentiometer.value)))
+    new_fader_value = fader.value # Get the current position
+    if new_fader_value != old_fader_value:
+        # Position has changed
+        if serial_debug:
+            print(new_fader_value)
+
+        # Fill the pixels a color based on the position of the fader.
+        pixels.fill(colorwheel(fader_to_color(new_fader_value)))
+        old_fader_value = new_fader_value
 
     ### NeoKey handling
     # Check each button, if pressed, light up the matching neopixel!
