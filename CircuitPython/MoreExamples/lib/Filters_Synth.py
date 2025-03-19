14ea09aa694f3d4663664525dbe38955ed2d445e
bobbyvivian <90727978+bobbyvivian@users.noreply.github.com>
2025-03-19 16:22:42 +0400

    Add files via upload

---
+++ CircuitPython/MoreExamples/lib/Filters_Synth.py
@@ -0,0 +1,72 @@
+# SPDX-FileCopyrightText: 2023 John Park and @todbot / Tod Kurt
+#
+# SPDX-License-Identifier: MIT
+
+import time
+import board
+import digitalio
+import audiomixer
+import synthio
+
+# for PWM audio with an RC filter
+# import audiopwmio
+# audio = audiopwmio.PWMAudioOut(board.GP10)
+
+# for I2S audio with external I2S DAC board
+import audiobusio
+
+# I2S on Audio BFF or Amp BFF on QT Py:
+# audio = audiobusio.I2SOut(bit_clock=board.A3, word_select=board.A2, data=board.A1)
+audio = audiobusio.I2SOut(board.A0, board.A1, board.A2)
+
+mixer = audiomixer.Mixer(channel_count=1, sample_rate=22050, buffer_size=2048)
+
+amp_env_slow = synthio.Envelope(
+                                attack_time=0.2,
+                                sustain_level=1.0,
+                                release_time=0.8
+)
+
+synth = synthio.Synthesizer(channel_count=1, sample_rate=22050, envelope=amp_env_slow)
+
+# set up filters
+frequency = 2000
+resonance = 1.5
+lpf = synth.low_pass_filter(frequency, resonance)
+hpf = synth.high_pass_filter(frequency, resonance)
+bpf = synth.band_pass_filter(frequency, resonance)
+
+note1 = synthio.Note(frequency=330, filter=None)
+
+audio.play(mixer)
+mixer.voice[0].play(synth)
+mixer.voice[0].level = 0.2
+
+while True:
+    # no filter
+    note1.filter = None
+    synth.press(note1)
+    time.sleep(1.25)
+    synth.release(note1)
+    time.sleep(1.25)
+
+    # lpf
+    note1.filter = lpf
+    synth.press(note1)
+    time.sleep(1.25)
+    synth.release(note1)
+    time.sleep(1.25)
+
+    # hpf
+    note1.filter = hpf
+    synth.press(note1)
+    time.sleep(1.25)
+    synth.release(note1)
+    time.sleep(1.25)
+
+    # bpf
+    note1.filter = bpf
+    synth.press(note1)
+    time.sleep(1.25)
+    synth.release(note1)
+    time.sleep(1.25)
